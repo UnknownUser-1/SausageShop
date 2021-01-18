a6465b513dda5a2dfd1e2afc0cfc0e68a5fb7508
FomaFom <54770976+FomaFom@users.noreply.github.com>
2021-01-18 17:40:05 +0400

    Разбираюсь с Fx

---
+++ src/main/java/sausegeShop/Fx/FxController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sausegeShop.Fx;
+
+/**
+ *
+ * @author pro56
+ */
+public class FxController {
+    public void yeaa(){
+        System.out.println("Сделано по-мясному!");
+    }
+}
