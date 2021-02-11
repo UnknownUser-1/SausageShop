81e5ec85da1896972b2ccdc95b262057f27fc0ff
Kostya <89277584951@mail.ru>
2021-02-11 17:36:14 +0400

    Починил рейтинг теперь вроде работает, делаю компараторы

---
+++ src/main/java/sausegeShop/comparators/NameComparator.java
@@ -0,0 +1,11 @@
+package sausegeShop.comparators;
+
+import java.util.Comparator;
+
+public class NameComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return 0;
+    }
+}
